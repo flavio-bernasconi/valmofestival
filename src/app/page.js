7add58ce9329048a83e80284326862542019ecf1
flavio <info@flaviobernasconi.it>
2025-04-19 13:27:53 +0200

    fix description

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ const nunito = Nunito({ subsets: ["latin"] });
 export const metadata = {
   title: "Valmofestival - 23/24/25 Maggio 2025",
   description:
-    "Il Valmofestival ritorna il 23/24/25 Maggio 2025, con la quindicesima edizione, ci vediiamo come sempre in piazza G. da Caversaccio",
+    "Il Valmofestival ritorna il 23/24/25 Maggio 2025, con la quindicesima edizione. Ci vediamo a Valmorea (Como) con tanta musica, cibo e molto altro. ",
   keywords: [
     "Valmofestival",
     "valmorea",
@@ -215,13 +215,39 @@ export default function Home() {
           </div>
         </section>
         <div className="container mx-auto p-8 text-center mt-20">
-          <p className="text-8xl font-light text-slate-100">Sponsor</p>
-          <p className="text-xl font-medium text-slate-100 mt-12">
+          <p className="text-8xl font-light text-slate-100 mb-12">Sponsor</p>
+          <p className="text-xl font-medium text-slate-100 flex gap-3 items-center justify-center">
+            <svg
+              aria-hidden="true"
+              className={cn(
+                "w-8 h-8 text-gray-100 animate-spin dark:text-gray-600 fill-orange-300"
+              )}
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
             Coming soon...
           </p>
         </div>
       </main>
-      <footer className=""></footer>
+      <footer className="mt-12">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1111.2928128553654!2d8.928802529828724!3d45.812466797219756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDXCsDQ4JzQ1LjAiTiA4wrA1NSc0NS40IkU!5e0!3m2!1sit!2sit!4v1745061689635!5m2!1sit!2sit"
+          width="100%"
+          height="500px"
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </footer>
     </div>
   );
 }
