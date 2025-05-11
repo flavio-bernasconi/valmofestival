a8283fa8358775ffa0dc20db24c2b0ea760a9bb4
flavio_bernasconi <info@flaviobernasconi.it>
2025-05-11 12:38:15 +0200

    fix menu

--- src/app/page.js
+++ src/app/page.js
@@ -181,7 +181,7 @@ export default function Home() {
                       <span className="text-xl font-bold bg-orange-400 text-white px-2 py-1 rounded-md">
                         NOVITÀ VEGAN
                       </span>{" "}
-                      Falafel + contorno | panino falafel
+                      Falafel + contorno | Falafel
                     </li>
                   </ul>
                 </MenuAccordion>
