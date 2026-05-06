51a8350af129409ee4f477b75ffec92f0ac4058f
Flavio Bernasconi <info@flaviobernasconi.it>
2026-05-06 09:16:00 +0200

    persistence posthog

--- src/providers/PosthogProvider.jsx
+++ src/providers/PosthogProvider.jsx
@@ -11,6 +11,7 @@ export function PostHogProvider({ children }) {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
       defaults: "2026-01-30",
       cookieless_mode: "always",
+      persistence: "localStorage",
     });
   }, []);
 
